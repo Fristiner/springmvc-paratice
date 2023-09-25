a4e4afac14b980238955278d13ecf6fdf2a357fc
peter-matc <2446454333@qq.com>
2023-09-25 17:21:51 +0800

    rm .idea

--- springmvc-base-quick-01/src/main/java/com/ma/config/MvcConfig.java
+++ springmvc-base-quick-01/src/main/java/com/ma/config/MvcConfig.java
@@ -18,18 +18,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+/**
+ * MVC 配置
+ *
+ * @author admin
+ * @date 2023/09/25
+ */
 @Configuration
 @ComponentScan("com.ma.controller")
 
 public class MvcConfig {
 
 
-
+    /**
+     * 处理程序映射
+     *
+     * @return 请求映射处理程序映射
+     */
     @Bean
     public RequestMappingHandlerMapping handlerMapping(){
         return new RequestMappingHandlerMapping();
     }
 
+    /**
+     * 处理程序适配器
+     *
+     * @return {@link RequestMappingHandlerAdapter}
+     */
     @Bean
     public RequestMappingHandlerAdapter handlerAdapter(){
         return new RequestMappingHandlerAdapter();
